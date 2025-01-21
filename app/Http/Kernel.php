4b14eb86c6a31f34fa90103686e49a5103b38bf3
Thamaraikannan <thamaraikannan07cse@gmail.com>
2025-01-21 10:50:38 +0530

    added

---
+++ app/Http/Kernel.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{ 
+
+protected $routeMiddleware = [
+    'isAdmin' => \App\Http\Middleware\IsAdmin::class,
+];
+
+protected $middlewareGroups = [
+    'api' => [
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        'throttle:api',
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ],
+];
+
+
+    public function handle($request)
+    {
+        return parent::handle($request);
+    }
+}
